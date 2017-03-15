2d9cda474a4866609326ab545dfb6ba5a842c6f4
Ilya Linnik <thenixan@gmail.com>
2017-03-15 18:22:19 +0300

    Changed JvmField to JvmStatic

--- app/src/main/java/ru/evotor/integrations/ReceiptsApi.kt
+++ app/src/main/java/ru/evotor/integrations/ReceiptsApi.kt
@@ -10,57 +10,57 @@ class ReceiptsApi {
 
     companion object {
 
-        @JvmField val AUTHORITY = "ru.evotor.receipt"
+        @JvmStatic val AUTHORITY = "ru.evotor.receipt"
 
-        @JvmField val BASE_URI = Uri.parse("content://$AUTHORITY")
+        @JvmStatic val BASE_URI = Uri.parse("content://$AUTHORITY")
 
     }
 
     object Description {
 
-        @JvmField val PATH_RECEIPT_DESCRIPTION = "information"
+        @JvmStatic val PATH_RECEIPT_DESCRIPTION = "information"
 
-        @JvmField val URI = Uri.withAppendedPath(BASE_URI, PATH_RECEIPT_DESCRIPTION)
+        @JvmStatic val URI = Uri.withAppendedPath(BASE_URI, PATH_RECEIPT_DESCRIPTION)
 
-        @JvmField val ROW_ID = "_id"
-        @JvmField val ROW_UUID = "uuid"
-        @JvmField val ROW_DISCOUNT = "discount"
+        @JvmStatic val ROW_ID = "_id"
+        @JvmStatic val ROW_UUID = "uuid"
+        @JvmStatic val ROW_DISCOUNT = "discount"
 
     }
 
     object Positions {
 
-        @JvmField val PATH_RECEIPT_POSITIONS = "positions"
+        @JvmStatic val PATH_RECEIPT_POSITIONS = "positions"
 
-        @JvmField val URI_RECEIPT_POSITIONS = Uri.withAppendedPath(BASE_URI, PATH_RECEIPT_POSITIONS)
+        @JvmStatic val URI_RECEIPT_POSITIONS = Uri.withAppendedPath(BASE_URI, PATH_RECEIPT_POSITIONS)
 
-        @JvmField val ROW_ID = "_id"
-        @JvmField val ROW_UUID = "uuid"
-        @JvmField val ROW_TYPE = "type"
-        @JvmField val ROW_CODE = "code"
-        @JvmField val ROW_MEASURE = "measure"
-        @JvmField val ROW_PRICE = "price"
-        @JvmField val ROW_QUANTITY = "quantity"
-        @JvmField val ROW_MARK = "mark"
-        @JvmField val ROW_NAME = "name"
+        @JvmStatic val ROW_ID = "_id"
+        @JvmStatic val ROW_UUID = "uuid"
+        @JvmStatic val ROW_TYPE = "type"
+        @JvmStatic val ROW_CODE = "code"
+        @JvmStatic val ROW_MEASURE = "measure"
+        @JvmStatic val ROW_PRICE = "price"
+        @JvmStatic val ROW_QUANTITY = "quantity"
+        @JvmStatic val ROW_MARK = "mark"
+        @JvmStatic val ROW_NAME = "name"
     }
 
     object Payments {
 
-        @JvmField val PATH_RECEIPT_PAYMENTS = "payments"
+        @JvmStatic val PATH_RECEIPT_PAYMENTS = "payments"
 
-        @JvmField val URI = Uri.withAppendedPath(BASE_URI, PATH_RECEIPT_PAYMENTS)
+        @JvmStatic val URI = Uri.withAppendedPath(BASE_URI, PATH_RECEIPT_PAYMENTS)
 
-        @JvmField val ROW_ID = "_id"
-        @JvmField val ROW_UUID = "uuid"
-        @JvmField val ROW_SUM = "sum"
-        @JvmField val ROW_TYPE = "type"
-        @JvmField val ROW_RRN = "rrn"
-        @JvmField val ROW_PIN_PAD_UUID = "pin_pad_uuid"
+        @JvmStatic val ROW_ID = "_id"
+        @JvmStatic val ROW_UUID = "uuid"
+        @JvmStatic val ROW_SUM = "sum"
+        @JvmStatic val ROW_TYPE = "type"
+        @JvmStatic val ROW_RRN = "rrn"
+        @JvmStatic val ROW_PIN_PAD_UUID = "pin_pad_uuid"
 
         object Type {
-            @JvmField val TYPE_CASH = 0
-            @JvmField val TYPE_CARD = 1
+            @JvmStatic val TYPE_CASH = 0
+            @JvmStatic val TYPE_CARD = 1
         }
 
     }
