ecd320430fd2fab240ce86f37ef5624d4ad0f100
xcam <a.kuznetsov@evotor.ru>
2017-03-15 16:38:36 +0300

    Add @JvmField for all constants

--- app/src/main/java/ru/evotor/integrations/users/RolesTable.kt
+++ app/src/main/java/ru/evotor/integrations/users/RolesTable.kt
@@ -3,8 +3,8 @@ package ru.evotor.integrations.users
 import android.net.Uri
 
 object RolesTable {
-    val URI = Uri.withAppendedPath(UsersContentProviderContract.BASE_URI, "roles")
+    @JvmField val URI = Uri.withAppendedPath(UsersContentProviderContract.BASE_URI, "roles")
 
-    val ROW_UUID = "UUID"
-    val ROW_TITLE = "TITLE"
+    @JvmField val ROW_UUID = "UUID"
+    @JvmField val ROW_TITLE = "TITLE"
 }
