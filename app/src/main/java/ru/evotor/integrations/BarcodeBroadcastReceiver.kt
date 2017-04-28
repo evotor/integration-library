30a47c8ffa07000afac67cc5e71f11ed8822411a
Ilya Linnik <thenixan@gmail.com>
2017-04-28 13:43:41 +0300

    Reworked barcode broadcast receiver

--- app/src/main/java/ru/evotor/integrations/BarcodeBroadcastReceiver.kt
+++ app/src/main/java/ru/evotor/integrations/BarcodeBroadcastReceiver.kt
@@ -1,47 +1,37 @@
 package ru.evotor.integrations
 
-import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import java.lang.NullPointerException
 
 /**
  * Created by nixan on 28.04.17.
  */
 
-private const val ACTION_SCANNED = "ru.evotor.devices.ScannedCode"
-private const val EXTRA_SCANNED_CODE = "ScannedCode"
 
-class BarcodeBroadcastReceiver : BroadcastReceiver() {
-
-    private val scannerListeners = arrayListOf<BarcodeScannerListener>()
-
-    fun register(activity: Activity?) {
-        if (activity == null) {
-            throw NullPointerException("Activity should not be null")
-        }
-        activity.registerReceiver(this, IntentFilter(ACTION_SCANNED))
-    }
-
-    fun unregister(activity: Activity?) {
-        if (activity == null) {
-            throw NullPointerException("Activity should not be null")
-        }
-        activity.unregisterReceiver(this)
-    }
+abstract class BarcodeBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         intent?.extras?.getString(EXTRA_SCANNED_CODE)?.let { code ->
             if (code.isNotEmpty()) {
-                scannerListeners.forEach { it.barcodeScanned(code) }
+                onReceiveBarcode(code)
             }
         }
     }
 
-}
+    abstract fun onReceiveBarcode(barcode: String)
 
-interface BarcodeScannerListener {
-    fun barcodeScanned(barcode: String)
+    companion object {
+
+        @JvmStatic
+        public val ACTION_SCANNED = "ru.evotor.devices.ScannedCode"
+
+        @JvmStatic
+        public val EXTRA_SCANNED_CODE = "ScannedCode"
+
+        @JvmStatic
+        public val BARCODE_INTENT_FILTER = IntentFilter(ACTION_SCANNED)
+    }
+
 }
