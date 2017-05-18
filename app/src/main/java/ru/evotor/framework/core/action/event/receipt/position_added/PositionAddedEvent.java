9cf7a365d53ddce005146d9db8be8eb8a916b8bb
xcam <a.kuznetsov@evotor.ru>
2017-05-18 18:44:27 +0300

    Position added event

---
+++ app/src/main/java/ru/evotor/framework/core/action/event/receipt/position_added/PositionAddedEvent.java
@@ -0,0 +1,11 @@
+package ru.evotor.framework.core.action.event.receipt.position_added;
+
+/**
+ * Created by a.kuznetsov on 18/05/2017.
+ */
+
+public interface PositionAddedEvent {
+    String BROADCAST_ACTION = "evotor.intent.action.receipt.sell.POSITION_ADDED";
+    String KEY_POSITION = "position";
+
+}
