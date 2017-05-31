e9c68e1b96000217953e5d59740955a7ae36b356
EVOTOR\o.mozgovoy <o.mozgovoy@evotor.ru>
2017-05-31 18:33:22 +0300

    только скидка для скидки на чек, никаких спиков изменений

--- app/src/main/java/ru/evotor/framework/core/action/event/receipt/discount/ReceiptDiscountEventResult.java
+++ app/src/main/java/ru/evotor/framework/core/action/event/receipt/discount/ReceiptDiscountEventResult.java
@@ -1,61 +1,40 @@
 package ru.evotor.framework.core.action.event.receipt.discount;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 import ru.evotor.framework.Utils;
-import ru.evotor.framework.core.action.datamapper.ChangesMapper;
-import ru.evotor.framework.core.action.event.receipt.changes.IChange;
-import ru.evotor.framework.core.action.event.receipt.changes.position.IPositionChange;
+
+import static ru.evotor.framework.calculator.MoneyCalculator.HUNDRED;
+import static ru.evotor.framework.calculator.MoneyCalculator.MONEY_PRECISION;
 
 public class ReceiptDiscountEventResult {
 
     private static final String KEY_RESULT = "result";
-    private static final String KEY_CHANGES = "changes";
     private static final String KEY_DISCOUNT = "discount";
 
     public static ReceiptDiscountEventResult create(Bundle bundle) {
         String resultName = bundle.getString(KEY_RESULT);
-        BigDecimal discount = new BigDecimal(bundle.getLong(KEY_DISCOUNT, 0)).divide(new BigDecimal(100), BigDecimal.ROUND_FLOOR);
-        Parcelable[] changesParcelable = bundle.getParcelableArray(KEY_CHANGES);
-        List<IChange> changes = ChangesMapper.INSTANCE.create(changesParcelable);
-        List<IPositionChange> positionChanges = new ArrayList<>();
-        for (IChange change : changes) {
-            if (change instanceof IPositionChange) {
-                positionChanges.add((IPositionChange) change);
-            }
-        }
+        BigDecimal discount = new BigDecimal(bundle.getLong(KEY_DISCOUNT, 0)).setScale(MONEY_PRECISION).divide(HUNDRED, BigDecimal.ROUND_FLOOR);
         return new ReceiptDiscountEventResult(
                 Utils.safeValueOf(Result.class, resultName, Result.UNKNOWN),
-                discount,
-                positionChanges
+                discount
         );
     }
 
     private final Result result;
     private final BigDecimal discount;
-    private final List<IPositionChange> changes;
 
-    public ReceiptDiscountEventResult(Result result, BigDecimal discount, List<IPositionChange> changes) {
+    public ReceiptDiscountEventResult(Result result, BigDecimal discount) {
         this.result = result;
         this.discount = discount;
-        this.changes = changes;
     }
 
     public Bundle toBundle() {
         Bundle bundle = new Bundle();
         bundle.putString(KEY_RESULT, result.name());
-        bundle.putLong(KEY_DISCOUNT, discount.multiply(new BigDecimal(100)).longValue());
-        Parcelable[] changesParcelable = new Parcelable[changes.size()];
-        for (int i = 0; i < changesParcelable.length; i++) {
-            IChange change = changes.get(i);
-            changesParcelable[i] = ChangesMapper.INSTANCE.toBundle(change);
-        }
-        bundle.putParcelableArray(KEY_CHANGES, changesParcelable);
+        bundle.putLong(KEY_DISCOUNT, discount.multiply(HUNDRED).longValue());
         return bundle;
     }
 
@@ -65,10 +44,6 @@ public class ReceiptDiscountEventResult {
 
     public Result getResult() {
         return result;
-    }
-
-    public List<IPositionChange> getChanges() {
-        return changes;
     }
 
     public enum Result {
