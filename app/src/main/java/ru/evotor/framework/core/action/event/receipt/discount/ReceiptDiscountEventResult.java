8d21b511435f8dbf82b58edf9535c6fa5c52ebff
EVOTOR\o.mozgovoy <o.mozgovoy@evotor.ru>
2017-06-15 08:08:06 +0300

    вернул модификации чека в момент задания скидки

--- app/src/main/java/ru/evotor/framework/core/action/event/receipt/discount/ReceiptDiscountEventResult.java
+++ app/src/main/java/ru/evotor/framework/core/action/event/receipt/discount/ReceiptDiscountEventResult.java
@@ -1,19 +1,26 @@
 package ru.evotor.framework.core.action.event.receipt.discount;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
+import ru.evotor.framework.Utils;
 import ru.evotor.framework.core.action.datamapper.BundleUtils;
+import ru.evotor.framework.core.action.datamapper.ChangesMapper;
+import ru.evotor.framework.core.action.event.receipt.changes.IChange;
+import ru.evotor.framework.core.action.event.receipt.changes.position.IPositionChange;
 import ru.evotor.framework.core.action.event.receipt.changes.position.SetExtra;
 
 public class ReceiptDiscountEventResult {
 
     private static final String KEY_DISCOUNT = "discount";
     private static final String KEY_RECEIPT_EXTRA = "extra";
+    private static final String KEY_CHANGES = "changes";
 
     @Nullable
     public static ReceiptDiscountEventResult create(@Nullable Bundle bundle) {
@@ -26,7 +33,11 @@ public class ReceiptDiscountEventResult {
         }
         return new ReceiptDiscountEventResult(
                 discount,
-                SetExtra.from(bundle.getBundle(KEY_RECEIPT_EXTRA))
+                SetExtra.from(bundle.getBundle(KEY_RECEIPT_EXTRA)),
+                Utils.filterByClass(
+                        ChangesMapper.INSTANCE.create(bundle.getParcelableArray(KEY_CHANGES)),
+                        IPositionChange.class
+                )
         );
     }
 
@@ -34,15 +45,19 @@ public class ReceiptDiscountEventResult {
     private final BigDecimal discount;
     @Nullable
     private final SetExtra extra;
+    @Nullable
+    private final List<IPositionChange> changes;
 
     public ReceiptDiscountEventResult(
             @NonNull BigDecimal discount,
-            @Nullable SetExtra extra
+            @Nullable SetExtra extra,
+            @Nullable List<IPositionChange> changes
     ) {
         Objects.requireNonNull(discount);
 
         this.discount = discount;
         this.extra = extra;
+        this.changes = changes;
     }
 
     @NonNull
@@ -50,6 +65,12 @@ public class ReceiptDiscountEventResult {
         Bundle bundle = new Bundle();
         bundle.putString(KEY_DISCOUNT, discount.toPlainString());
         bundle.putBundle(KEY_RECEIPT_EXTRA, extra == null ? null : extra.toBundle());
+        Parcelable[] changesParcelable = new Parcelable[changes == null ? 0 : changes.size()];
+        for (int i = 0; i < changesParcelable.length; i++) {
+            IChange change = changes.get(i);
+            changesParcelable[i] = ChangesMapper.INSTANCE.toBundle(change);
+        }
+        bundle.putParcelableArray(KEY_CHANGES, changesParcelable);
         return bundle;
     }
 
@@ -61,5 +82,10 @@ public class ReceiptDiscountEventResult {
     @Nullable
     public SetExtra getExtra() {
         return extra;
+    }
+    
+    @Nullable
+    public List<IPositionChange> getChanges() {
+        return changes;
     }
 }
