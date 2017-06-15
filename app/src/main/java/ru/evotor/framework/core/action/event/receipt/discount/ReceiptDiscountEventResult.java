4838879edc15818302001e776bb036b570f0b07b
EVOTOR\o.mozgovoy <o.mozgovoy@evotor.ru>
2017-06-15 08:04:52 +0300

    merge-problem fix

--- app/src/main/java/ru/evotor/framework/core/action/event/receipt/discount/ReceiptDiscountEventResult.java
+++ app/src/main/java/ru/evotor/framework/core/action/event/receipt/discount/ReceiptDiscountEventResult.java
@@ -1,30 +1,19 @@
 package ru.evotor.framework.core.action.event.receipt.discount;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Objects;
 
-<<<<<<< HEAD
-import ru.evotor.framework.Utils;
-import ru.evotor.framework.calculator.MoneyCalculator;
-import ru.evotor.framework.core.action.datamapper.ChangesMapper;
-import ru.evotor.framework.core.action.event.receipt.changes.IChange;
-import ru.evotor.framework.core.action.event.receipt.changes.position.IPositionChange;
-=======
 import ru.evotor.framework.core.action.datamapper.BundleUtils;
->>>>>>> develop
 import ru.evotor.framework.core.action.event.receipt.changes.position.SetExtra;
 
 public class ReceiptDiscountEventResult {
 
     private static final String KEY_DISCOUNT = "discount";
     private static final String KEY_RECEIPT_EXTRA = "extra";
-    private static final String KEY_CHANGES = "changes";
 
     @Nullable
     public static ReceiptDiscountEventResult create(@Nullable Bundle bundle) {
@@ -37,11 +26,7 @@ public class ReceiptDiscountEventResult {
         }
         return new ReceiptDiscountEventResult(
                 discount,
-                SetExtra.from(bundle.getBundle(KEY_RECEIPT_EXTRA)),
-                Utils.filterByClass(
-                        ChangesMapper.INSTANCE.create(bundle.getParcelableArray(KEY_CHANGES)),
-                        IPositionChange.class
-                )
+                SetExtra.from(bundle.getBundle(KEY_RECEIPT_EXTRA))
         );
     }
 
@@ -49,19 +34,15 @@ public class ReceiptDiscountEventResult {
     private final BigDecimal discount;
     @Nullable
     private final SetExtra extra;
-    @Nullable
-    private final List<IPositionChange> changes;
 
     public ReceiptDiscountEventResult(
             @NonNull BigDecimal discount,
-            @Nullable SetExtra extra,
-            @Nullable List<IPositionChange> changes
+            @Nullable SetExtra extra
     ) {
         Objects.requireNonNull(discount);
 
         this.discount = discount;
         this.extra = extra;
-        this.changes = changes;
     }
 
     @NonNull
@@ -69,12 +50,6 @@ public class ReceiptDiscountEventResult {
         Bundle bundle = new Bundle();
         bundle.putString(KEY_DISCOUNT, discount.toPlainString());
         bundle.putBundle(KEY_RECEIPT_EXTRA, extra == null ? null : extra.toBundle());
-        Parcelable[] changesParcelable = new Parcelable[changes == null ? 0 : changes.size()];
-        for (int i = 0; i < changesParcelable.length; i++) {
-            IChange change = changes.get(i);
-            changesParcelable[i] = ChangesMapper.INSTANCE.toBundle(change);
-        }
-        bundle.putParcelableArray(KEY_CHANGES, changesParcelable);
         return bundle;
     }
 
@@ -87,17 +62,4 @@ public class ReceiptDiscountEventResult {
     public SetExtra getExtra() {
         return extra;
     }
-<<<<<<< HEAD
-
-    @Nullable
-    public List<IPositionChange> getChanges() {
-        return changes;
-    }
-
-    public enum Result {
-        OK,
-        UNKNOWN;
-    }
-=======
->>>>>>> develop
 }
