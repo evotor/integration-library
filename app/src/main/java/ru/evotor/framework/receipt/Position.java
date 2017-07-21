49d10076cb20106735177ad4a22e579b6b3d82c2
xcam <xcam@users.noreply.github.com>
2017-07-21 13:38:58 +0300

    Фикс чтения чека с позициями по свободной цене (#76)
    * Исправил проблему чтения оплат из чека
    
    * Исправил баг с чтением чека, который содержит позицию по свободной цене
    
    * Javadoc + annotation

--- app/src/main/java/ru/evotor/framework/receipt/Position.java
+++ app/src/main/java/ru/evotor/framework/receipt/Position.java
@@ -25,10 +25,12 @@ public class Position implements Parcelable {
     /**
      * UUID товара.
      */
+    @Nullable
     private String productUuid;
     /**
      * Код товара.
      */
+    @Nullable
     private String productCode;
     /**
      * Вид товара.
@@ -61,6 +63,7 @@ public class Position implements Parcelable {
     /**
      * Штрихкод, по которому товар был найден.
      */
+    @Nullable
     private String barcode;
     /**
      * Алкогольная марка.
@@ -69,14 +72,17 @@ public class Position implements Parcelable {
     /**
      * Крепость.
      */
+    @Nullable
     private BigDecimal alcoholByVolume;
     /**
      * Код вида продукции ФСРАР.
      */
+    @Nullable
     private Long alcoholProductKindCode;
     /**
      * Объём тары.
      */
+    @Nullable
     private BigDecimal tareVolume;
     /**
      * Экстра ключи
@@ -89,8 +95,8 @@ public class Position implements Parcelable {
 
     public Position(
             String uuid,
-            String productUuid,
-            String productCode,
+            @Nullable String productUuid,
+            @Nullable String productCode,
             ProductType productType,
             String name,
             String measureName,
@@ -98,11 +104,11 @@ public class Position implements Parcelable {
             BigDecimal price,
             BigDecimal priceWithDiscountPosition,
             BigDecimal quantity,
-            String barcode,
+            @Nullable String barcode,
             String mark,
-            BigDecimal alcoholByVolume,
-            Long alcoholProductKindCode,
-            BigDecimal tareVolume,
+            @Nullable BigDecimal alcoholByVolume,
+            @Nullable Long alcoholProductKindCode,
+            @Nullable BigDecimal tareVolume,
             Set<ExtraKey> extraKeys,
             List<Position> subPositions
     ) {
@@ -214,70 +220,128 @@ public class Position implements Parcelable {
         return sum;
     }
 
+    /**
+     * @return UUID позиции
+     */
     public String getUuid() {
         return uuid;
     }
 
+    /**
+     * @return UUID товара.
+     */
+    @Nullable
     public String getProductUuid() {
         return productUuid;
     }
 
+    /**
+     * @return Код товара.
+     */
+    @Nullable
     public String getProductCode() {
         return productCode;
     }
 
+    /**
+     * @return Вид товара.
+     */
     public ProductType getProductType() {
         return productType;
     }
 
+    /**
+     * @return Наименование
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return Наименование единицы измерения.
+     */
     public String getMeasureName() {
         return measureName;
     }
 
+    /**
+     * @return Точность единицы измерения.
+     */
     public int getMeasurePrecision() {
         return measurePrecision;
     }
 
+    /**
+     * @return Цена без скидок.
+     */
     public BigDecimal getPrice() {
         return price;
     }
 
+    /**
+     * @return Цена с учетом скидки на позицию.
+     */
     public BigDecimal getPriceWithDiscountPosition() {
         return priceWithDiscountPosition;
     }
 
+    /**
+     * @return Количество.
+     */
     public BigDecimal getQuantity() {
         return quantity;
     }
 
+    /**
+     * @return Штрихкод, по которому товар был найден.
+     */
+    @Nullable
     public String getBarcode() {
         return barcode;
     }
 
+    /**
+     * @return Алкогольная марка.
+     */
+    @Nullable
     public String getMark() {
         return mark;
     }
 
+    /**
+     * @return Крепость.
+     */
+    @Nullable
     public BigDecimal getAlcoholByVolume() {
         return alcoholByVolume;
     }
 
+    /**
+     * @return Код вида продукции ФСРАР.
+     */
+    @Nullable
     public Long getAlcoholProductKindCode() {
         return alcoholProductKindCode;
     }
 
+    /**
+     * @return Объём тары.
+     */
+    @Nullable
     public BigDecimal getTareVolume() {
         return tareVolume;
     }
 
+    /**
+     * @return Экстра ключи.
+     */
     public Set<ExtraKey> getExtraKeys() {
         return extraKeys;
     }
 
+    /**
+     * @return Подпозиции (модификаторы).
+     */
     public List<Position> getSubPositions() {
         return subPositions;
     }
