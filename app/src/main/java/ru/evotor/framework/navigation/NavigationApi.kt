6304b20b6f7bb376fe7ad84e06e663f5c6b5a822
Alexandra <a.ermilova@evotor.ru>
2017-11-23 15:03:18 +0300

    Add edit commodity intent extra constants

--- app/src/main/java/ru/evotor/framework/navigation/NavigationApi.kt
+++ app/src/main/java/ru/evotor/framework/navigation/NavigationApi.kt
@@ -1,6 +1,7 @@
 package ru.evotor.framework.navigation
 
 import android.content.Intent
+import android.os.Bundle
 
 object NavigationApi {
     const val BROADCAST_ACTION_EDIT_SELL = "evotor.intent.action.edit.SELL"
@@ -11,8 +12,26 @@ object NavigationApi {
     const val BROADCAST_ACTION_REPORT_CASH_REGISTER = "evotor.intent.action.report.CASH_REGISTER"
     const val BROADCAST_ACTION_EDIT_COMMODITY = "evotor.intent.action.edit.COMMODITY"
 
-    const val EXTRA_IN_COMMODITY_UUID = "inCommodityUuid"
-    const val EXTRA_OUT_COMMODITY_UUID = "outCommodityUuid"
+    // extras for edit commodity intent
+    const val EXTRA_COMMODITY_UUID = "commodityUuid"
+    const val EXTRA_TYPE = "type"
+    const val EXTRA_PARENT_UUID = "parentUuid"
+    const val EXTRA_ENABLE_AUTOGENERATE_ABBREVIATION = "enableAutoGenerateAbbreviation"
+    const val EXTRA_COMMODITY_ID = "commodityId"
+    const val EXTRA_MULTIPLY_ADDING = "multiplyAdding"
+    const val EXTRA_SCROLL_TO_SWITCH_OFF_SELL_FORBIDDEN = "scrollToSwitchOffSellForbidden"
+    const val EXTRA_NAME = "name"
+    const val EXTRA_COMMODITY_CODE_STRING = "commodityCodeString"
+    const val EXTRA_BARCODE_DATA = "barcodeData"
+    const val EXTRA_BARCODE = "barcode"
+    const val EXTRA_ALCO_CODE = "alcoCode"
+    const val EXTRA_PRICE = "price"
+    const val EXTRA_MEASURE_ID = "measureId"
+    const val EXTRA_MEASURE_NAME = "measureName"
+    const val EXTRA_ALCOHOL_BY_VOLUME = "alcoholByVolume"
+    const val EXTRA_ALCOHOL_PRODUCT_KIND_CODE = "alcoholProductKindCode"
+    const val EXTRA_TARE_VOLUME = "tareVolume"
+    const val EXTRA_REQUEST_FROM_CLOUD = "requestFromCloud";
 
     /**
      * форма наполнения чека продажи
@@ -66,8 +85,8 @@ object NavigationApi {
      * форма редактора товара
      */
     @JvmStatic
-    fun createIntentForEditCommodity(uuid: String?): Intent {
-        return Intent(BROADCAST_ACTION_EDIT_COMMODITY).apply { uuid?.let { putExtra(EXTRA_IN_COMMODITY_UUID, uuid) } }
+    fun createIntentForEditCommodity(bundle: Bundle?): Intent {
+        return Intent(BROADCAST_ACTION_EDIT_COMMODITY).apply { bundle?.let { putExtras(bundle) } }
     }
 
 }
