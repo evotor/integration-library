d7046e7b5ab15c70171bd975b047e26b9bbb63a5
d.fabrichnyi <d.fabrichnyi@evotor.ru>
2018-02-06 15:51:58 +0300

    Added getLastSessionNumber and isSessionOpened api

---
+++ src/main/java/ru/evotor/framework/system/LastSessionNumber.kt
@@ -0,0 +1,30 @@
+package ru.evotor.framework.system
+
+import android.database.Cursor
+import android.net.Uri
+
+/**
+ * Created by d.fabrichnyi on 06.02.2018.
+ */
+class LastSessionNumber private constructor(val sessionNumber: Long) {
+    companion object {
+        val COLUMN_NAME = "LAST_SESSION_NUMBER"
+        val LAST_SESSION_NUMBER_PATH = "LastSessionNumber"
+        val URI = Uri.withAppendedPath(SystemStateApi.BASE_URI, LAST_SESSION_NUMBER_PATH)
+
+        fun fromCursor(cursor: Cursor): LastSessionNumber? {
+            val valueStr = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
+            if (valueStr.isNullOrEmpty()) {
+                return null
+            }
+
+            val value = try {
+                valueStr.toLong()
+            } catch (error: NumberFormatException) {
+                return null
+            }
+
+            return LastSessionNumber(value)
+        }
+    }
+}
---
+++ src/main/java/ru/evotor/framework/system/SystemStateApi.kt
@@ -0,0 +1,49 @@
+package ru.evotor.framework.system
+
+import android.content.Context
+import android.net.Uri
+
+/**
+ * Created by d.fabrichnyi on 06.02.2018.
+ */
+object SystemStateApi {
+
+    @JvmField
+    val BASE_URI = Uri.parse("content://ru.evotor.evotorpos.system_state")
+
+    @JvmStatic
+    fun getLastSessionNumber(context: Context): LastSessionNumber? {
+        val cursor = context.contentResolver.query(LastSessionNumber.URI, null,
+                null, null, null)
+
+        if (cursor == null) {
+            return null
+        }
+
+        return cursor.use {
+            if (it.moveToFirst()) {
+                return@use LastSessionNumber.fromCursor(it)
+            } else {
+                return@use null
+            }
+        }
+    }
+
+    @JvmStatic
+    fun isSessionOpened(context: Context): IsSessionOpened? {
+        val cursor = context.contentResolver.query(IsSessionOpened.URI, null,
+                null, null, null)
+
+        if (cursor == null) {
+            return null
+        }
+
+        return cursor.use {
+            if (it.moveToFirst()) {
+                return@use IsSessionOpened.fromCursor(it)
+            } else {
+                return@use null
+            }
+        }
+    }
+}
