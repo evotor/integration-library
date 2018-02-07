574bd5d521a5fc61b4981c1ea67ddd5c754a7613
d.fabrichnyi <d.fabrichnyi@evotor.ru>
2018-02-07 16:23:03 +0300

    Added getLastSessionNumber and isSessionOpened api

---
+++ src/main/java/ru/evotor/framework/system/IsSessionOpened.kt
@@ -0,0 +1,27 @@
+package ru.evotor.framework.system
+
+import android.database.Cursor
+import android.net.Uri
+
+class IsSessionOpened private constructor(val isOpened: Boolean) {
+    companion object {
+        val COLUMN_NAME = "IS_SESSION_OPENED"
+        val IS_SESSION_OPENED_PATH = "IsSessionOpened"
+        val URI = Uri.withAppendedPath(SystemStateApi.BASE_URI, IS_SESSION_OPENED_PATH)
+
+        fun fromCursor(cursor: Cursor): IsSessionOpened? {
+            val valueStr = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
+            if (valueStr.isNullOrEmpty()) {
+                return null
+            }
+
+            val value = try {
+                valueStr.toBoolean()
+            } catch (error: NumberFormatException) {
+                return null
+            }
+
+            return IsSessionOpened(value)
+        }
+    }
+}
