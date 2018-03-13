205751e679d068f9de38dd286f839b751c69f7cb
Луньков Артем Дмитриевич <a.lunkov@evotor.local>
2018-03-13 14:57:33 +0300

    ProductMapper

--- src/main/java/ru/evotor/framework/Cursor.kt
+++
@@ -1,9 +0,0 @@
-package ru.evotor.framework
-
-import android.database.Cursor
-
-
-abstract class Cursor<T>(cursor: Cursor) : android.database.Cursor by cursor {
-
-    abstract fun getValue(): T
-}
