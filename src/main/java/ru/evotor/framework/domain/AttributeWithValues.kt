a270fdb872d43275d6fe65542fc58c46b0e1da4b
khaustov1 <la2haste@gmail.com>
2018-07-09 19:30:44 +0300

    Update AttributeWithValues.kt

--- src/main/java/ru/evotor/framework/domain/AttributeWithValues.kt
+++ src/main/java/ru/evotor/framework/domain/AttributeWithValues.kt
@@ -32,23 +32,23 @@ data class AttributeWithValues(
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(version)
         // Determine position in parcel for writing data size
-        val dataSizePosition = parcel.dataPosition();
+        val dataSizePosition = parcel.dataPosition()
         // Use integer placeholder for data size
-        parcel.writeInt(0);
+        parcel.writeInt(0)
         //Determine position of data start
-        val startDataPosition = parcel.dataPosition();
+        val startDataPosition = parcel.dataPosition()
         parcel.writeString(uuid)
         parcel.writeString(name)
         parcel.writeTypedList(attributeValues)
         // Calculate data size
-        val dataSize = parcel.dataPosition() - startDataPosition;
+        val dataSize = parcel.dataPosition() - startDataPosition
         // Save position at the end of data
-        val endOfDataPosition = parcel.dataPosition();
+        val endOfDataPosition = parcel.dataPosition()
         //Set position to start to write data size
-        parcel.setDataPosition(dataSizePosition);
-        parcel.writeInt(dataSize);
+        parcel.setDataPosition(dataSizePosition)
+        parcel.writeInt(dataSize)
         // Go back to the end of parcel
-        parcel.setDataPosition(endOfDataPosition);
+        parcel.setDataPosition(endOfDataPosition)
     }
 
     override fun describeContents(): Int = 0
