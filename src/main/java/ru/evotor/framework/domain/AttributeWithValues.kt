324a96e3d67cc28d78e448df8e9e26824483b462
khaustov1 <la2haste@gmail.com>
2018-07-09 19:09:43 +0300

    Update AttributeWithValues.kt

--- src/main/java/ru/evotor/framework/domain/AttributeWithValues.kt
+++ src/main/java/ru/evotor/framework/domain/AttributeWithValues.kt
@@ -32,23 +32,23 @@ data class AttributeWithValues(
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(version)
         // Determine position in parcel for writing data size
-        int dataSizePosition = dest.dataPosition();
+        int dataSizePosition = parcel.dataPosition();
         // Use integer placeholder for data size
-        dest.writeInt(0);
+        parcel.writeInt(0);
         //Determine position of data start
-        int startDataPosition = dest.dataPosition();
+        int startDataPosition = parcel.dataPosition();
         parcel.writeString(uuid)
         parcel.writeString(name)
         parcel.writeTypedList(attributeValues)
         // Calculate data size
-        int dataSize = dest.dataPosition() - startDataPosition;
+        int dataSize = parcel.dataPosition() - startDataPosition;
         // Save position at the end of data
-        int endOfDataPosition = dest.dataPosition();
+        int endOfDataPosition = parcel.dataPosition();
         //Set position to start to write data size
-        dest.setDataPosition(dataSizePosition);
-        dest.writeInt(dataSize);
+        parcel.setDataPosition(dataSizePosition);
+        parcel.writeInt(dataSize);
         // Go back to the end of parcel
-        dest.setDataPosition(endOfDataPosition);
+        parcel.setDataPosition(endOfDataPosition);
     }
 
     override fun describeContents(): Int = 0
