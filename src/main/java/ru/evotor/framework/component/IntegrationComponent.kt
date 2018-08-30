c4e4eb633dd8dd9ee1bacb6c7cdc07a0793e37d5
Alexandra <a.ermilova@evotor.ru>
2018-08-30 12:37:01 +0300

    Implement payment performer and integration component parcelable versioning

--- src/main/java/ru/evotor/framework/component/IntegrationComponent.kt
+++ src/main/java/ru/evotor/framework/component/IntegrationComponent.kt
@@ -25,22 +25,55 @@ open class IntegrationComponent(
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
+        /**
+         * NOTE: When adding fields in the process of updating this API, make sure to bump
+         * {@link #PARCELABLE_VERSION}.
+         */
+        parcel.writeInt(PARCELABLE_VERSION)
+        // Inject a placeholder that will store the parcel size from this point on
+        // (not including the size itself).
+        val sizePosition = parcel.dataPosition()
+        parcel.writeInt(0)
+        val startPosition = parcel.dataPosition()
+
+        // version 1
         parcel.writeString(packageName)
         parcel.writeString(componentName)
         parcel.writeString(appUuid)
         parcel.writeString(appName)
+
+        // Go back and write the size
+        val parcelableSize = parcel.dataPosition() - startPosition
+        parcel.setDataPosition(sizePosition)
+        parcel.writeInt(parcelableSize)
+        parcel.setDataPosition(startPosition + parcelableSize)
     }
 
     override fun describeContents(): Int {
         return 0
     }
 
-
     companion object {
+        /**
+         * Since there might be a case where new versions of the client using the library getting
+         * old versions of the protocol (and thus old versions of this class), we need a versioning
+         * system for the parcels sent between the clients and the providers.
+         */
+        const val PARCELABLE_VERSION = 1
+
         @JvmField
         val CREATOR: Parcelable.Creator<IntegrationComponent> = object : Parcelable.Creator<IntegrationComponent> {
             override fun createFromParcel(parcel: Parcel): IntegrationComponent {
-                return IntegrationComponent(parcel)
+                val parcelableVersion = parcel.readInt()
+                val parcelableSize = parcel.readInt()
+                val startPosition = parcel.dataPosition()
+
+                val component = IntegrationComponent(parcel)
+
+                // skip over all fields added in future versions of this parcel
+                parcel.setDataPosition(startPosition + parcelableSize)
+
+                return component
             }
 
             override fun newArray(size: Int): Array<IntegrationComponent?> {
--- src/main/java/ru/evotor/framework/component/PaymentPerformer.kt
+++ src/main/java/ru/evotor/framework/component/PaymentPerformer.kt
@@ -28,11 +28,29 @@ class PaymentPerformer(val paymentSystem: PaymentSystem?,
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
+        /**
+         * NOTE: When adding fields in the process of updating this API, make sure to bump
+         * {@link #PARCELABLE_VERSION}.
+         */
+        parcel.writeInt(PARCELABLE_VERSION)
+        // Inject a placeholder that will store the parcel size from this point on
+        // (not including the size itself).
+        val sizePosition = parcel.dataPosition()
+        parcel.writeInt(0)
+        val startPosition = parcel.dataPosition()
+
+        // version 1
         parcel.writeParcelable(paymentSystem, flags)
         parcel.writeString(packageName)
         parcel.writeString(componentName)
         parcel.writeString(appUuid)
         parcel.writeString(appName)
+
+        // Go back and write the size
+        val parcelableSize = parcel.dataPosition() - startPosition
+        parcel.setDataPosition(sizePosition)
+        parcel.writeInt(parcelableSize)
+        parcel.setDataPosition(startPosition + parcelableSize)
     }
 
     override fun describeContents(): Int {
@@ -40,10 +58,26 @@ class PaymentPerformer(val paymentSystem: PaymentSystem?,
     }
 
     companion object {
+        /**
+         * Since there might be a case where new versions of the client using the library getting
+         * old versions of the protocol (and thus old versions of this class), we need a versioning
+         * system for the parcels sent between the clients and the providers.
+         */
+        const val PARCELABLE_VERSION = 1
+
         @JvmField
         val CREATOR: Parcelable.Creator<PaymentPerformer> = object : Parcelable.Creator<PaymentPerformer> {
             override fun createFromParcel(parcel: Parcel): PaymentPerformer {
-                return PaymentPerformer(parcel)
+                val parcelableVersion = parcel.readInt()
+                val parcelableSize = parcel.readInt()
+                val startPosition = parcel.dataPosition()
+
+                val performer = PaymentPerformer(parcel)
+
+                // skip over all fields added in future versions of this parcel
+                parcel.setDataPosition(startPosition + parcelableSize)
+
+                return performer
             }
 
             override fun newArray(size: Int): Array<PaymentPerformer?> {
