aec1f8b9acc8852f964846ecbb8aa8d67bd52d4f
ArtemiyDmtrvch <a.lunkov@evotor.ru>
2018-10-30 17:35:57 +0300

    updated kotlin

--- src/main/java/ru/evotor/framework/receipt/position/provider/AgentRequisitesContract.kt
+++ src/main/java/ru/evotor/framework/receipt/position/provider/AgentRequisitesContract.kt
@@ -1,6 +1,6 @@
 package ru.evotor.framework.receipt.position.provider
 
-object AgentRequisitesTable {
+object AgentRequisitesContract {
     const val COLUMN_POSITION_UUID = "POSITION_UUID"
     const val COLUMN_AGENT_UUID = "AGENT_UUID"
     const val COLUMN_AGENT_TYPE = "AGENT_TYPE"
