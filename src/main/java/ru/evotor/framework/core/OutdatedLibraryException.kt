9710af6cd094d6b5eaef62525930506b910624a9
ArtemiyDmtrvch <35735328+ArtemiyDmtrvch@users.noreply.github.com>
2018-12-03 13:49:40 +0300

    STMNGMNT-285 STMNGMNT-284 (#188)
    * outdated library exception
    
    * runtime exception
    
    * zhvezdo4ka

---
+++ src/main/java/ru/evotor/framework/core/OutdatedLibraryException.kt
@@ -0,0 +1,13 @@
+package ru.evotor.framework.core
+
+private const val CAUSE = "*"
+
+private const val BASE_MESSAGE = "Found $CAUSE that is not supported in current integration-library. Update your integration-library to resolve this error."
+
+/**
+ * Исключение, возникающее при расхождении протокола взаимодействия между сторонним приложенем,
+ * которое использует устаревшую версию integration-library, и системным приложением СТ.
+ *
+ * При обновлении integration-library в стороннем приложении исключение должно перестать возникать.
+ */
+class OutdatedLibraryException internal constructor(cause: String) : RuntimeException(BASE_MESSAGE.replace(CAUSE, cause))
