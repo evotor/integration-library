9d29beb6b46227744d9d3dfab005c87e3a47b351
Alexandra <a.ermilova@evotor.ru>
2019-04-02 11:34:40 +0300

    STDEV-1735: Implement payment delegator api

---
+++ src/main/java/ru/evotor/framework/component/PaymentDelegatorApi.kt
@@ -0,0 +1,54 @@
+package ru.evotor.framework.component
+
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import ru.evotor.framework.core.action.event.receipt.payment.combined.event.PaymentDelegatorEvent
+
+object PaymentDelegatorApi {
+
+    private const val METADATA_NAME_APP_UUID = "app_uuid"
+
+    fun getAllPaymentDelegators(packageManager: PackageManager): List<PaymentDelegator> {
+        val eventName = PaymentDelegatorEvent.NAME_ACTION
+        val applicationsList = ArrayList<PaymentDelegator>()
+        val intent = Intent(eventName)
+        val applicationsInfo = packageManager.queryIntentServices(intent, PackageManager.GET_META_DATA)
+        for (resolveInfo in applicationsInfo) {
+            generatePaymentDelegator(packageManager, resolveInfo)?.let {
+                applicationsList.add(it)
+            }
+        }
+        return applicationsList
+    }
+
+    private fun generatePaymentDelegator(packageManager: PackageManager, resolveInfo: ResolveInfo): PaymentDelegator? {
+        if (resolveInfo.serviceInfo == null || resolveInfo.serviceInfo.metaData == null) {
+            return null
+        }
+        val appUuid: String?
+        try {
+            val packageInfo = packageManager.getPackageInfo(resolveInfo.serviceInfo.packageName, PackageManager.GET_META_DATA or PackageManager.GET_PERMISSIONS)
+            if (!hasPermission(packageInfo)) return null
+            appUuid = getAppUuid(packageInfo)
+        } catch (exc: PackageManager.NameNotFoundException) {
+            return null
+        }
+        appUuid ?: return null
+
+        return PaymentDelegator(
+                resolveInfo.serviceInfo.packageName,
+                resolveInfo.serviceInfo.name,
+                appUuid,
+                resolveInfo.loadLabel(packageManager).toString()
+        )
+    }
+
+    private fun hasPermission(packageInfo: PackageInfo) = packageInfo.requestedPermissions.contains(PaymentDelegatorEvent.NAME_PERMISSION)
+
+    private fun getAppUuid(packageInfo: PackageInfo) =
+            if (packageInfo.applicationInfo.metaData != null)
+                packageInfo.applicationInfo.metaData.getString(METADATA_NAME_APP_UUID, null)
+            else null
+}
