3699fdb2a08b5100106bc7bf1dcd8049f941aab5
a.bogdanov <a.bogdanov@evotor.ru>
2019-04-25 15:13:21 +0300

    STDEV-1851 - новая редакция задачи

--- src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/ReceiptFormationIntegrationService.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/ReceiptFormationIntegrationService.kt
@@ -3,36 +3,22 @@ package ru.evotor.framework.receipt.formation.event.handler.service
 import android.os.Bundle
 import ru.evotor.IBundlable
 import ru.evotor.framework.common.event.handler.service.IntegrationServiceV2
-import ru.evotor.framework.receipt.formation.event.AddingPositionByBarcodeRequestedEvent
-import ru.evotor.framework.receipt.formation.event.CreateProductByBarcodeEvent
-import ru.evotor.framework.receipt.formation.event.CreatingProductIntentByBarcodeRequestedEvent
+import ru.evotor.framework.receipt.formation.event.ReturnPositionsForBarcodeRequestedEvent
 
 abstract class ReceiptFormationIntegrationService(
-        private val actionBarcodeReceived: String,
-        private val actionCreateProductByBarcode: String,
-        private val actionSelectedForCreatingProduct: String
+        private val actionBarcodeReceived: String
 ) : IntegrationServiceV2() {
 
     final override fun onEvent(action: String, bundle: Bundle): IBundlable? {
         when (action) {
             actionBarcodeReceived -> {
-                return handleEvent(AddingPositionByBarcodeRequestedEvent.from(bundle)
-                        ?: return null)
-            }
-            actionCreateProductByBarcode -> {
-                return handleEvent(CreatingProductIntentByBarcodeRequestedEvent.from(bundle)
-                        ?: return null)
-            }
-            actionSelectedForCreatingProduct -> {
-                handleEvent(CreateProductByBarcodeEvent.from(bundle)
+                return handleEvent(ReturnPositionsForBarcodeRequestedEvent.from(bundle)
                         ?: return null)
             }
         }
         return null
     }
 
-    open fun handleEvent(event: AddingPositionByBarcodeRequestedEvent): AddingPositionByBarcodeRequestedEvent.Result? = null
-    open fun handleEvent(event: CreatingProductIntentByBarcodeRequestedEvent): CreatingProductIntentByBarcodeRequestedEvent.Result? = null
-    open fun handleEvent(event: CreateProductByBarcodeEvent): Unit = Unit
+    open fun handleEvent(event: ReturnPositionsForBarcodeRequestedEvent): ReturnPositionsForBarcodeRequestedEvent.Result? = null
 
 }
--- src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/SellIntegrationService.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/SellIntegrationService.kt
@@ -1,28 +1,16 @@
 package ru.evotor.framework.receipt.formation.event.handler.service
 
 import ru.evotor.framework.core.RequiresIntentAction
-import ru.evotor.framework.receipt.formation.event.AddingPositionByBarcodeRequestedEvent
-import ru.evotor.framework.receipt.formation.event.CreateProductByBarcodeEvent
-import ru.evotor.framework.receipt.formation.event.CreatingProductIntentByBarcodeRequestedEvent
+import ru.evotor.framework.receipt.formation.event.ReturnPositionsForBarcodeRequestedEvent
 
-abstract class SellIntegrationService: ReceiptFormationIntegrationService(
-        ACTION_BARCODE_RECEIVED,
-        ACTION_CREATE_PRODUCT_BY_BARCODE,
-        ACTION_SELECTED_FOR_CREATING_PRODUCT
+abstract class SellIntegrationService : ReceiptFormationIntegrationService(
+        ACTION_BARCODE_RECEIVED
 ) {
 
     @RequiresIntentAction(ACTION_BARCODE_RECEIVED)
-    override fun handleEvent(event: AddingPositionByBarcodeRequestedEvent): AddingPositionByBarcodeRequestedEvent.Result? = null
-
-    @RequiresIntentAction(ACTION_CREATE_PRODUCT_BY_BARCODE)
-    override fun handleEvent(event: CreatingProductIntentByBarcodeRequestedEvent): CreatingProductIntentByBarcodeRequestedEvent.Result? = null
-
-    @RequiresIntentAction(ACTION_SELECTED_FOR_CREATING_PRODUCT)
-    override fun handleEvent(event: CreateProductByBarcodeEvent): Unit = Unit
+    override fun handleEvent(event: ReturnPositionsForBarcodeRequestedEvent): ReturnPositionsForBarcodeRequestedEvent.Result? = null
 
     companion object {
         const val ACTION_BARCODE_RECEIVED = "ru.evotor.event.sell.BARCODE_RECEIVED"
-        const val ACTION_CREATE_PRODUCT_BY_BARCODE = "ru.evotor.event.sell.CREATE_PRODUCT_BY_BARCODE"
-        const val ACTION_SELECTED_FOR_CREATING_PRODUCT = "ru.evotor.event.sell.SELECTED_FOR_CREATING_PRODUCT"
     }
 }
