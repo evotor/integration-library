314c6899f140a04accc84ab3cf014cf7b9b3cd80
a.bogdanov <a.bogdanov@evotor.ru>
2019-07-16 12:02:20 +0300

    STDEV-2463 - добавил точку интеграции на показ кнопки заполнения реквизитов покупателя

---
+++ src/main/java/ru/evotor/framework/receipt/formation/event/InputPurchaserRequisitesEvent.kt
@@ -0,0 +1,23 @@
+package ru.evotor.framework.receipt.formation.event
+
+import android.os.Bundle
+import ru.evotor.framework.common.event.IntegrationEvent
+
+data class InputPurchaserRequisitesEvent(
+        val receiptUuid: String
+) : IntegrationEvent() {
+
+    override fun toBundle() = Bundle().apply {
+        putString(KEY_RECEIPT_UUID, receiptUuid)
+    }
+
+    companion object {
+        const val KEY_RECEIPT_UUID = "KEY_RECEIPT_UUID"
+
+        @JvmStatic
+        fun from(bundle: Bundle?) = bundle?.let {
+            InputPurchaserRequisitesEvent(it.getString(KEY_RECEIPT_UUID))
+        }
+    }
+
+}
--- src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/SellIntegrationService.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/SellIntegrationService.kt
@@ -1,8 +1,10 @@
 package ru.evotor.framework.receipt.formation.event.handler.service
 
 import android.os.Bundle
+import ru.evotor.IBundlable
 import ru.evotor.framework.common.event.handler.service.IntegrationServiceV2
 import ru.evotor.framework.core.RequiresIntentAction
+import ru.evotor.framework.receipt.formation.event.InputPurchaserRequisitesEvent
 import ru.evotor.framework.receipt.formation.event.ReturnPositionsForBarcodeRequestedEvent
 import ru.evotor.framework.receipt.formation.event.ReturnPurchaserRequisitesForPrintGroupRequestedEvent
 
@@ -11,6 +13,7 @@ abstract class SellIntegrationService : IntegrationServiceV2() {
     final override fun onEvent(action: String, bundle: Bundle) = when (action) {
         ACTION_BARCODE_RECEIVED -> ReturnPositionsForBarcodeRequestedEvent.from(bundle)?.let { handleEvent(it) }
         ACTION_PURCHASER_REQUISITES -> ReturnPurchaserRequisitesForPrintGroupRequestedEvent.from(bundle)?.let { handleEvent(it) }
+        ACTION_PURCHASER_REQUISITES_INPUT -> InputPurchaserRequisitesEvent.from(bundle)?.let { handleEvent(it) }
         else -> null
     }
 
@@ -20,9 +23,13 @@ abstract class SellIntegrationService : IntegrationServiceV2() {
     @RequiresIntentAction(ACTION_PURCHASER_REQUISITES)
     open fun handleEvent(event: ReturnPurchaserRequisitesForPrintGroupRequestedEvent): ReturnPurchaserRequisitesForPrintGroupRequestedEvent.Result? = null
 
+    @RequiresIntentAction(ACTION_PURCHASER_REQUISITES_INPUT)
+    open fun handleEvent(event: InputPurchaserRequisitesEvent): IBundlable? = null
+
     companion object {
         const val ACTION_BARCODE_RECEIVED = "ru.evotor.event.sell.BARCODE_RECEIVED"
         const val ACTION_PURCHASER_REQUISITES = "ru.evotor.event.sell.PURCHASER_REQUISITES"
+        const val ACTION_PURCHASER_REQUISITES_INPUT = "ru.evotor.event.sell.PURCHASER_REQUISITES_INPUT"
 
         const val PERMISSION = "ru.evotor.permission.SELL_INTEGRATION_SERVICE"
     }
