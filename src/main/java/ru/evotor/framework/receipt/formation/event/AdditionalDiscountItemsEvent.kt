d3c86da7b49c9a0cc5a715a8a3a99fb0b88dff65
a.bogdanov <a.bogdanov@evotor.ru>
2019-08-22 09:44:11 +0300

    STDEV-2463 - event теперь общий, принимает action на вход

--- src/main/java/ru/evotor/framework/receipt/formation/event/AdditionalDiscountItemsEvent.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/AdditionalDiscountItemsEvent.kt
@@ -3,20 +3,23 @@ package ru.evotor.framework.receipt.formation.event
 import android.os.Bundle
 import ru.evotor.framework.common.event.IntegrationEvent
 
-data class InputPurchaserRequisitesEvent(
-        val receiptUuid: String
+data class AdditionalDiscountItemsEvent(
+        val receiptUuid: String,
+        val action: String
 ) : IntegrationEvent() {
 
     override fun toBundle() = Bundle().apply {
         putString(KEY_RECEIPT_UUID, receiptUuid)
+        putString(KEY_ACTION, action)
     }
 
     companion object {
         const val KEY_RECEIPT_UUID = "KEY_RECEIPT_UUID"
+        const val KEY_ACTION = "KEY_ACTION"
 
         @JvmStatic
         fun from(bundle: Bundle?) = bundle?.let {
-            InputPurchaserRequisitesEvent(it.getString(KEY_RECEIPT_UUID))
+            AdditionalDiscountItemsEvent(it.getString(KEY_RECEIPT_UUID), it.getString(KEY_ACTION))
         }
     }
 
--- src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/SellIntegrationService.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/SellIntegrationService.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import ru.evotor.IBundlable
 import ru.evotor.framework.common.event.handler.service.IntegrationServiceV2
 import ru.evotor.framework.core.RequiresIntentAction
-import ru.evotor.framework.receipt.formation.event.InputPurchaserRequisitesEvent
+import ru.evotor.framework.receipt.formation.event.AdditionalDiscountItemsEvent
 import ru.evotor.framework.receipt.formation.event.ReturnPositionsForBarcodeRequestedEvent
 import ru.evotor.framework.receipt.formation.event.ReturnPurchaserRequisitesForPrintGroupRequestedEvent
 
@@ -13,7 +13,7 @@ abstract class SellIntegrationService : IntegrationServiceV2() {
     final override fun onEvent(action: String, bundle: Bundle) = when (action) {
         ACTION_BARCODE_RECEIVED -> ReturnPositionsForBarcodeRequestedEvent.from(bundle)?.let { handleEvent(it) }
         ACTION_PURCHASER_REQUISITES -> ReturnPurchaserRequisitesForPrintGroupRequestedEvent.from(bundle)?.let { handleEvent(it) }
-        ACTION_PURCHASER_REQUISITES_INPUT -> InputPurchaserRequisitesEvent.from(bundle)?.let { handleEvent(it) }
+        ACTION_ADDITIONAL_DISCOUNT_ITEMS -> AdditionalDiscountItemsEvent.from(bundle)?.let { handleEvent(it) }
         else -> null
     }
 
@@ -23,13 +23,13 @@ abstract class SellIntegrationService : IntegrationServiceV2() {
     @RequiresIntentAction(ACTION_PURCHASER_REQUISITES)
     open fun handleEvent(event: ReturnPurchaserRequisitesForPrintGroupRequestedEvent): ReturnPurchaserRequisitesForPrintGroupRequestedEvent.Result? = null
 
-    @RequiresIntentAction(ACTION_PURCHASER_REQUISITES_INPUT)
-    open fun handleEvent(event: InputPurchaserRequisitesEvent): IBundlable? = null
+    @RequiresIntentAction(ACTION_ADDITIONAL_DISCOUNT_ITEMS)
+    open fun handleEvent(event: AdditionalDiscountItemsEvent): IBundlable? = null
 
     companion object {
         const val ACTION_BARCODE_RECEIVED = "ru.evotor.event.sell.BARCODE_RECEIVED"
         const val ACTION_PURCHASER_REQUISITES = "ru.evotor.event.sell.PURCHASER_REQUISITES"
-        const val ACTION_PURCHASER_REQUISITES_INPUT = "ru.evotor.event.sell.PURCHASER_REQUISITES_INPUT"
+        const val ACTION_ADDITIONAL_DISCOUNT_ITEMS = "ru.evotor.event.sell.ADDITIONAL_DISCOUNT_ITEMS"
 
         const val PERMISSION = "ru.evotor.permission.SELL_INTEGRATION_SERVICE"
     }
