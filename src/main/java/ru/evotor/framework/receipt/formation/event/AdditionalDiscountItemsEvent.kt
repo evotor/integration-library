f5cb5d2cdfc5b3964ffa3d09561a952e3d534951
a.bogdanov <a.bogdanov@evotor.ru>
2019-08-22 12:18:49 +0300

    STDEV-2463 - убрал action, не нужен он

--- src/main/java/ru/evotor/framework/receipt/formation/event/AdditionalDiscountItemsEvent.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/AdditionalDiscountItemsEvent.kt
@@ -4,22 +4,19 @@ import android.os.Bundle
 import ru.evotor.framework.common.event.IntegrationEvent
 
 data class AdditionalDiscountItemsEvent(
-        val receiptUuid: String,
-        val action: String
+        val receiptUuid: String
 ) : IntegrationEvent() {
 
     override fun toBundle() = Bundle().apply {
         putString(KEY_RECEIPT_UUID, receiptUuid)
-        putString(KEY_ACTION, action)
     }
 
     companion object {
         const val KEY_RECEIPT_UUID = "KEY_RECEIPT_UUID"
-        const val KEY_ACTION = "KEY_ACTION"
 
         @JvmStatic
         fun from(bundle: Bundle?) = bundle?.let {
-            AdditionalDiscountItemsEvent(it.getString(KEY_RECEIPT_UUID), it.getString(KEY_ACTION))
+            AdditionalDiscountItemsEvent(it.getString(KEY_RECEIPT_UUID))
         }
     }
 
