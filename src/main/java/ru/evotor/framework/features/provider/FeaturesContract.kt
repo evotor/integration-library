2a7d96486b5c5dbf6f7ee8e9c7ac2636ebcb48b6
a.bogdanov <a.bogdanov@evotor.ru>
2019-09-10 20:11:53 +0300

    STDEV-2767 - API для работы с кассой * добавлено получение состояния опции "Развозная торговля" * оптимизировано API для работы с платными обновлениями

---
+++ src/main/java/ru/evotor/framework/features/provider/FeaturesContract.kt
@@ -0,0 +1,14 @@
+package ru.evotor.framework.features.provider
+
+object FeaturesContract {
+    const val BASE_PATH = "content://ru.evotor.paidupdates.FeaturesContentProvider"
+
+    const val PATH_VAT20 = "vat20"
+    const val PATH_FFD105 = "ffd105"
+    const val PATH_TOBACCOMARK = "tobaccomark"
+    const val PATH_PURCHASER = "purchaser"
+    const val PATH_DELIVERY = "delivery"
+
+    const val COLUMN_IS_ACTIVE = "is_active"
+
+}
