f56af25c762e461581bae259fd2ac2357d7ca553
a.bogdanov <a.bogdanov@evotor.ru>
2019-09-18 17:50:07 +0300

    STDEV-1892 - предзаполненность полей

--- src/main/java/ru/evotor/framework/receipt/formation/event/ReturnDeliveryRequisitesForReceiptRequestedEvent.kt
+++ src/main/java/ru/evotor/framework/receipt/formation/event/ReturnDeliveryRequisitesForReceiptRequestedEvent.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import ru.evotor.framework.common.event.IntegrationEvent
 
 data class ReturnDeliveryRequisitesForReceiptRequestedEvent(
-        val receiptUuid: String
+        val receiptUuid: String,
+        val paymentAddress: String? = null,
+        val paymentPlace: String? = null
 ) : IntegrationEvent() {
 
     override fun toBundle() = Bundle().apply {
         putString(KEY_RECEIPT_UUID, receiptUuid)
+        putString(KEY_EXTRA_ADDRESS, paymentAddress)
+        putString(KEY_RECEIPT_UUID, paymentPlace)
     }
 
     companion object {
         const val KEY_RECEIPT_UUID = "KEY_RECEIPT_UUID"
+        const val KEY_EXTRA_ADDRESS = "KEY_EXTRA_ADDRESS"
+        const val KEY_EXTRA_PLACE = "KEY_EXTRA_PLACE"
 
         @JvmStatic
         fun from(bundle: Bundle?) = bundle?.let {
             ReturnDeliveryRequisitesForReceiptRequestedEvent(
-                    receiptUuid = it.getString(KEY_RECEIPT_UUID)
-                            ?: return null)
+                    receiptUuid = it.getString(KEY_RECEIPT_UUID) ?: return null,
+                    paymentAddress = it.getString(KEY_EXTRA_ADDRESS),
+                    paymentPlace = it.getString(KEY_EXTRA_PLACE)
+            )
         }
     }
 
@@ -33,8 +41,6 @@ data class ReturnDeliveryRequisitesForReceiptRequestedEvent(
         }
 
         companion object {
-            const val KEY_EXTRA_ADDRESS = "KEY_EXTRA_ADDRESS"
-            const val KEY_EXTRA_PLACE = "KEY_EXTRA_PLACE"
 
             @JvmStatic
             fun from(bundle: Bundle?) = bundle?.let {
