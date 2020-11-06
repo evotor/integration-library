9f5a7311f565915ec37bd94ee7b07f5ff19a9ed3
Affordall <affordall@gmail.com>
2020-11-06 15:43:47 +0300

    STDEV-6425
    - MedicineAdditionalDetails is now data class

--- src/main/java/ru/evotor/framework/receipt/MedicineAdditionalDetails.kt
+++ src/main/java/ru/evotor/framework/receipt/MedicineAdditionalDetails.kt
@@ -7,7 +7,7 @@ import ru.evotor.IBundlable
 import ru.evotor.framework.ParcelableUtils
 import java.util.*
 
-class MedicineAdditionalDetails(
+data class MedicineAdditionalDetails(
         /**
          * Номер документа, не более 200 символов;
          * Составная часть дополнительного реквизита пользователя (тег 1086)
