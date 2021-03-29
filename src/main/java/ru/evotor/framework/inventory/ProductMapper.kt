54348b11def792bfef869015f80379d5ddbf4a16
v.degtev <m.degtev@evotor.ru>
2021-03-29 12:42:27 +0300

    STDEV-6963
    - product mapper stub

--- src/main/java/ru/evotor/framework/inventory/ProductMapper.kt
+++ src/main/java/ru/evotor/framework/inventory/ProductMapper.kt
@@ -37,7 +37,9 @@ internal object ProductMapper {
                         alcoholProductKindCode = cursor.getLong(cursor.getColumnIndex(ProductTable.ROW_ALCOHOL_PRODUCT_KIND_CODE)),
                         tareVolume = cursor.getLong(cursor.getColumnIndex(ProductTable.ROW_TARE_VOLUME)).let { BigDecimal(it).divide(BigDecimal(1000)) },
                         taxNumber = Utils.safeValueOf(TaxNumber::class.java, cursor.getString(cursor.getColumnIndex(ProductTable.ROW_TAX_NUMBER)), TaxNumber.NO_VAT),
-                        classificationCode = cursor.optString(cursor.getColumnIndex(ProductTable.ROW_CLASSIFICATION_CODE))
+                        classificationCode = cursor.optString(cursor.getColumnIndex(ProductTable.ROW_CLASSIFICATION_CODE)),
+                        // TODO: Only test
+                        partialSaleFrequency = ProductItem.PartialSaleFrequency.SOMETIMES
                 )
             }
         } catch (e: Exception) {
