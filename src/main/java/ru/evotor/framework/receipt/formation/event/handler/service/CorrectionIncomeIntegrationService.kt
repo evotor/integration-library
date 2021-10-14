dedec8ce8580e94a7bc19fe2aa2f20bbf27f7a93
Roman Egorov <devabladablah@gmail.com>
2021-10-14 13:39:56 +0700

    STDEV-9481 сервисы для экрана скидок чеков коррекции

---
+++ src/main/java/ru/evotor/framework/receipt/formation/event/handler/service/CorrectionIncomeIntegrationService.kt
@@ -0,0 +1,51 @@
+package ru.evotor.framework.receipt.formation.event.handler.service
+
+import android.os.Bundle
+import ru.evotor.framework.common.event.handler.service.IntegrationServiceV2
+import ru.evotor.framework.core.RequiresIntentAction
+import ru.evotor.framework.receipt.formation.event.DiscountScreenAdditionalItemsEvent
+import ru.evotor.framework.receipt.formation.event.ReturnPurchaserRequisitesForPrintGroupRequestedEvent
+
+/**
+ * Служба для работы с чеком коррекции.
+ */
+abstract class CorrectionIncomeIntegrationService : IntegrationServiceV2() {
+
+    final override fun onEvent(action: String, bundle: Bundle) = when (action) {
+        ACTION_PURCHASER_REQUISITES -> ReturnPurchaserRequisitesForPrintGroupRequestedEvent.from(bundle)?.let { handleEvent(it) }
+        ACTION_DISCOUNT_SCREEN_ADDITIONAL_ITEMS -> DiscountScreenAdditionalItemsEvent.from(bundle)?.let { handleEvent(it) }
+        else -> null
+    }
+
+    /**
+     * Возвращает смарт-терминалу массив печатных групп с соответствующими реквизитами покупателя.
+     *
+     * @param event Событие, с помощью которого смарт-терминал запрашивает у установленных приложений реквизиты покупателя.
+     * @return Результат обработки события, содержащий массив печатных групп и соответствующих им объектов с реквизитами покупателя.
+     * @see <a href="https://developer.evotor.ru/docs/doc_java_purchase_requisites_event_processing.html">Добавление реквизитов покупателя в чек</a>
+     */
+    @RequiresIntentAction(ACTION_PURCHASER_REQUISITES)
+    open fun handleEvent(event: ReturnPurchaserRequisitesForPrintGroupRequestedEvent): ReturnPurchaserRequisitesForPrintGroupRequestedEvent.Result? = null
+
+    /**
+     * Запускает приложение по нажатию кнопки на экране оплаты чека.
+     */
+    @RequiresIntentAction(ACTION_DISCOUNT_SCREEN_ADDITIONAL_ITEMS)
+    open fun handleEvent(event: DiscountScreenAdditionalItemsEvent): Nothing? = null
+
+    companion object {
+        /**
+         * Запрос [реквизитов покупателя][ru.evotor.framework.receipt.Purchaser] для добавления в чек коррекции.
+         *
+         * Чтобы подписать службу на получение запроса, в манифесте приложения, в элементе `action` intent-фильтра службы, укажите значение `ru.evotor.event.sell.PURCHASER_REQUISITES`.
+         */
+        const val ACTION_PURCHASER_REQUISITES = "ru.evotor.event.correction.income.PURCHASER_REQUISITES"
+        const val ACTION_DISCOUNT_SCREEN_ADDITIONAL_ITEMS = "ru.evotor.event.correction.income.DISCOUNT_SCREEN_ADDITIONAL_ITEMS"
+        /**
+         * Разрешение необходимое приложению для работы со службой [ru.evotor.framework.receipt.formation.event.handler.service.CorrectionIncomeIntegrationService].
+         *
+         * Чтобы выдать разрешение, в элементе `uses-permission` манифеста приложения, укажите значение `ru.evotor.permission.CORRECTION_INCOME_INTEGRATION_SERVICE`.
+         */
+        const val PERMISSION = "ru.evotor.permission.CORRECTION_INCOME_INTEGRATION_SERVICE"
+    }
+}
