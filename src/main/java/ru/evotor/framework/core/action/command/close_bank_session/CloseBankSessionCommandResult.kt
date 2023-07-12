41cb204926fd62e6a2ea9e618b98e29371e4092c
Вадим Богачев <v.bogachev@evotor.ru>
2023-07-12 12:03:15 +0700

    STDEV-14924

--- src/main/java/ru/evotor/framework/core/action/command/close_bank_session/CloseBankSessionCommandResult.kt
+++ src/main/java/ru/evotor/framework/core/action/command/close_bank_session/CloseBankSessionCommandResult.kt
@@ -16,41 +16,34 @@ class CloseBankSessionCommandResult(
     companion object {
 
         /**
-         * Отсутствует необходимое обновление для работы по ФФД 1.2.
-         * Чтобы его получить, установите из Эвотор.Маркета приложение «Маркировка»,
-         * если торгуете маркированным товаром, или «Смарт-терминал Плюс» в остальных случаях.
-         */
-        const val ERROR_CODE_FFD_12_FRAUD_DETECTED = -1
-
-        /**
          * ККМ в данный момент выполняет другую операцию
          */
-        const val ERROR_CODE_KKM_IS_BUSY = -2
+        const val ERROR_CODE_KKM_IS_BUSY = -1
 
         /**
          * Нет авторизованного пользователя на терминале
          */
-        const val ERROR_CODE_NO_AUTHENTICATED_USER = -3
+        const val ERROR_CODE_NO_AUTHENTICATED_USER = -2
 
         /**
          * У приложения нет необходимого разрешения (permission)
          */
-        const val ERROR_CODE_NO_PERMISSION = -4
+        const val ERROR_CODE_NO_PERMISSION = -3
 
         /**
          * Ошибка закрытия смены банковского терминала
          */
-        const val ERROR_CODE_CLOSE_PINPAD_SESSION_ERROR = -5
+        const val ERROR_CODE_CLOSE_PINPAD_SESSION_ERROR = -4
 
         /**
          * Пользователь не найден на терминале
          */
-        const val ERROR_CODE_USER_NOT_FOUND = -6
+        const val ERROR_CODE_USER_NOT_FOUND = -5
 
         /**
          * Платежная система с заданным accountId не найдена
          */
-        const val ERROR_CODE_PINPAD_NOT_FOUND = -7
+        const val ERROR_CODE_PINPAD_NOT_FOUND = -6
 
         private const val KEY_SLIP_LINES = "slipLines"
 
