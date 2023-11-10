f249492297ee211e7ea6821af47a4701854d8477
anton bogdanov <a.bogdanov@evotor.ru>
2023-11-10 11:47:13 +0300

    STDEV-16601 - форсированный логин, без запроса пина (для КСО)

--- src/main/java/ru/evotor/framework/users/UsersTable.kt
+++ src/main/java/ru/evotor/framework/users/UsersTable.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 object UsersTable {
     @JvmField val URI: Uri = Uri.withAppendedPath(UsersContentProviderContract.BASE_URI, "users")
     @JvmField val URI_AUTHENTICATED: Uri = Uri.withAppendedPath(URI, "authenticated")
+    @JvmField val URI_FORCE_AUTHENTICATED: Uri = Uri.withAppendedPath(URI_AUTHENTICATED, "force")
 
     const val ROW_USER_UUID = "USER_UUID"
     const val ROW_USER_SECOND_NAME = "USER_SECOND_NAME"
