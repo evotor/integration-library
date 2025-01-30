3f56b6095468cc5cfbbb3c1f28c1b7c7cbe4e43a
Ilya Fomenko <i.fomenko@evotor.ru>
2025-01-30 12:54:55 +0500

    STDEV-21064 Избавление от лишнего объекта маппера

--- src/main/java/ru/evotor/framework/core/action/event/receipt/jewelry/SendJewelryEvent.kt
+++ src/main/java/ru/evotor/framework/core/action/event/receipt/jewelry/SendJewelryEvent.kt
@@ -3,7 +3,6 @@ package ru.evotor.framework.core.action.event.receipt.jewelry
 import android.os.Bundle
 import ru.evotor.IBundlable
 import ru.evotor.framework.Utils
-import ru.evotor.framework.core.action.datamapper.JewelryMarkedPositionsMapper
 import ru.evotor.framework.core.action.event.receipt.payment.system.event.PaymentSystemEvent.OperationType
 import java.math.BigDecimal
 
@@ -34,7 +33,7 @@ class SendJewelryEvent(
         val result = Bundle()
         result.putString(KEY_RECEIPT_UUID, receiptUuid)
         result.putString(KEY_OPERATION_TYPE, operationType.name)
-        result.putBundle(KEY_JEWELRY_MARKED_POSITIONS, JewelryMarkedPositionsMapper.toBundle(jewelryMarkedPositions))
+        result.putBundle(KEY_JEWELRY_MARKED_POSITIONS, positionsToBundle(jewelryMarkedPositions))
         result.putLong(KEY_SESSION_NUMBER, sessionNumber)
         result.putLong(KEY_DOCUMENT_NUMBER, documentNumber)
         result.putInt(KEY_PAPER_WIDTH, paperWidth)
@@ -81,7 +80,7 @@ class SendJewelryEvent(
             )
 
         private fun getJewelryMarkedPositions(bundle: Bundle): Map<String, BigDecimal>? =
-            JewelryMarkedPositionsMapper.fromBundle(bundle.getBundle(KEY_JEWELRY_MARKED_POSITIONS))
+            bundleToPositions(bundle.getBundle(KEY_JEWELRY_MARKED_POSITIONS))
 
         private fun getSessionNumber(bundle: Bundle): Long? =
             bundle.getLong(KEY_SESSION_NUMBER, -1L).let { value ->
@@ -100,6 +99,30 @@ class SendJewelryEvent(
 
         private fun getKktSerialNumber(bundle: Bundle): String? =
             bundle.getString(KEY_KKT_SERIAL_NUMBER, null)
+
+        private fun bundleToPositions(positions: Bundle?): Map<String, BigDecimal>? {
+            positions?.let { bundle ->
+                bundle.classLoader = BigDecimal::class.java.classLoader
+                return HashMap<String, BigDecimal>().apply {
+                    positions.keySet().forEach { key ->
+                        val value = positions.getSerializable(key) as BigDecimal?
+                        value?.let {
+                            this[key] = it
+                        }
+                    }
+                }
+            } ?: return null
+        }
+
+        private fun positionsToBundle(positions: Map<String, BigDecimal>?): Bundle? {
+            positions?.let {
+                return Bundle().apply {
+                    positions.keys.forEach {
+                        this.putSerializable(it, positions[it])
+                    }
+                }
+            } ?: return null
+        }
     }
 
 }
