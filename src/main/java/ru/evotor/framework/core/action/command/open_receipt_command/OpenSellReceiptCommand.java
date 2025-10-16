3c15e2d020e81a1059528a427f97a64376e4cefe
Ilya Fomenko <i.fomenko@evotor.ru>
2025-10-16 16:02:37 +0500

    STDEV-23301

--- src/main/java/ru/evotor/framework/core/action/command/open_receipt_command/OpenSellReceiptCommand.java
+++ src/main/java/ru/evotor/framework/core/action/command/open_receipt_command/OpenSellReceiptCommand.java
@@ -22,6 +22,7 @@ import ru.evotor.framework.core.action.datamapper.ChangesMapper;
 import ru.evotor.framework.core.action.event.receipt.changes.IChange;
 import ru.evotor.framework.core.action.event.receipt.changes.position.PositionAdd;
 import ru.evotor.framework.core.action.event.receipt.changes.receipt.SetExtra;
+import ru.evotor.framework.core.action.event.receipt.changes.receipt.SetInternetRequisites;
 import ru.evotor.framework.core.action.event.receipt.changes.receipt.SetPurchaserContactData;
 
 /**
@@ -33,6 +34,7 @@ public class OpenSellReceiptCommand implements IBundlable {
     private static final String KEY_CHANGES = "changes";
     private static final String KEY_RECEIPT_EXTRA = "extra";
     private static final String KEY_RECEIPT_SET_PURCHASER_CONTACT_DATA = "setPurchaserContactData";
+    private static final String KEY_RECEIPT_SET_INTERNET_REQUISITES = "setInternetRequisites";
 
     @Nullable
     public static OpenSellReceiptCommand create(@Nullable Bundle bundle) {
@@ -46,7 +48,8 @@ public class OpenSellReceiptCommand implements IBundlable {
                         PositionAdd.class
                 ),
                 SetExtra.from(bundle.getBundle(KEY_RECEIPT_EXTRA)),
-                SetPurchaserContactData.from(bundle.getBundle(KEY_RECEIPT_SET_PURCHASER_CONTACT_DATA))
+                SetPurchaserContactData.from(bundle.getBundle(KEY_RECEIPT_SET_PURCHASER_CONTACT_DATA)),
+                SetInternetRequisites.from(bundle.getBundle(KEY_RECEIPT_SET_INTERNET_REQUISITES))
         );
     }
 
@@ -56,9 +59,11 @@ public class OpenSellReceiptCommand implements IBundlable {
     private final SetExtra extra;
     @Nullable
     private final SetPurchaserContactData setPurchaserContactData;
+    @Nullable
+    private final SetInternetRequisites setInternetRequisites;
 
     /**
-     * Используйте конструктор с setPurchaserContactData
+     * Используйте конструктор с setPurchaserContactData и setInternetRequisites
      *
      * @param changes
      * @param extra
@@ -76,12 +81,22 @@ public class OpenSellReceiptCommand implements IBundlable {
             @Nullable SetExtra extra,
             @Nullable SetPurchaserContactData setPurchaserContactData
     ) {
+        this(changes, extra, setPurchaserContactData, null);
+    }
+
+    public OpenSellReceiptCommand(
+            @Nullable List<PositionAdd> changes,
+            @Nullable SetExtra extra,
+            @Nullable SetPurchaserContactData setPurchaserContactData,
+            @Nullable SetInternetRequisites setInternetRequisites
+    ) {
         this.changes = new ArrayList<>();
         if (changes != null) {
             this.changes.addAll(changes);
         }
         this.extra = extra;
         this.setPurchaserContactData = setPurchaserContactData;
+        this.setInternetRequisites = setInternetRequisites;
     }
 
     public void process(@NonNull final Activity activity, IntegrationManagerCallback callback) {
@@ -117,6 +132,10 @@ public class OpenSellReceiptCommand implements IBundlable {
                 KEY_RECEIPT_SET_PURCHASER_CONTACT_DATA,
                 setPurchaserContactData == null ? null : setPurchaserContactData.toBundle()
         );
+        bundle.putBundle(
+                KEY_RECEIPT_SET_INTERNET_REQUISITES,
+                setInternetRequisites == null ? null : setInternetRequisites.toBundle()
+        );
         return bundle;
     }
 
@@ -133,5 +152,10 @@ public class OpenSellReceiptCommand implements IBundlable {
     @Nullable
     public SetPurchaserContactData getSetPurchaserContactData() {
         return setPurchaserContactData;
+    }
+
+    @Nullable
+    public SetInternetRequisites getSetInternetRequisites() {
+        return setInternetRequisites;
     }
 }
